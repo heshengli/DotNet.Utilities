c37a377e712701b03e42131b9f977ef858467b70
H <hsl1991@live.com>
2018-07-29 09:47:09 +0800

    init

---
+++ DotNet.Utilities/DotNet.Utilities.Security.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNet.Utilities.Security.Common;
+
+namespace DotNet.Utilities.Security.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Md5.Md5Encrypts("123456");
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
